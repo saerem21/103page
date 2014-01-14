335c0667e1d393847d9b67fb0f3daeda31e29eed
saerem21 <nemessisdds>
2014-01-14 12:09:41 +0900

    테이블 뷰 디비 예제 수정가능 버전입니다.
    완료 했습니다.

---
+++ 103page/Movie.h
@@ -0,0 +1,15 @@
+//
+//  Movie.h
+//  103page
+//
+//  Created by SDT-1 on 2014. 1. 13..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Movie : NSObject
+@property (copy, nonatomic) NSString *title;
+@property int rowID;
+
+@end
---
+++ 103page/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  103page
+//
+//  Created by SDT-1 on 2014. 1. 13..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource>
+
+@end
